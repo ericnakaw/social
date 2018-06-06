f821d0484403fd86a08cb7e20c8328c8cf5e7954
Eric Nakawatase <e.nakawatase@coblue.com.br>
2018-06-06 13:05:43 -0300

    5. Diretórios do Projeto.mp4

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,1 +1,21 @@
-continuar video 5 8:55 min
+<template>
+  <div class="">
+      <h2>Social</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
