71cd6e5a11ee4a47393ca6c48c4083bdb52b745a
Eric Nakawatase <e.nakawatase@coblue.com.br>
2018-06-20 22:31:13 -0300

    15. Cartão de Conteúdo - Parte 2

--- src/components/social/CardConteudoVue.vue
+++ src/components/social/CardConteudoVue.vue
@@ -4,22 +4,15 @@
     <div class="card-content">
       <div class="row valign-wrapper">
         <grid-vue tamanho="1">
-          <img src="https://materializecss.com/images/yuna.jpg" alt="" class="circle responsive-img"> <!-- notice the "circle" class -->
+          <img :src="perfil" :alt="nome" class="circle responsive-img">
         </grid-vue>
         <grid-vue tamanho="11">
           <span class="black-text">
-            <strong>Maria Silva</strong> - <small>20/06/2018 22:10</small>
+            <strong>{{nome}}</strong> - <small>{{data}}</small>
           </span>
         </grid-vue>
       </div>
-      <div class="card-image">
-        <img src="https://materializecss.com/images/sample-1.jpg">
-        <span class="card-title">Card Title</span>
-      </div>
-      <div class="card-content">
-        <p>I am a very simple card. I am good at containing small bits of information.
-        I am convenient because I require little markup to use effectively.</p>
-      </div>
+      <slot></slot>
     </div>
     <div class="card-action">
       <p>
@@ -38,7 +31,7 @@
     components:{
       GridVue
     },
-    props: [],
+    props: ['perfil','nome','data'],
     data() {
       return {}
     }
--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,20 +1,29 @@
 <template>
-  <card-conteudo-vue>
-      
+  <card-conteudo-vue perfil="https://materializecss.com/images/yuna.jpg" nome="Maria Silva" data="20/06/2018 22:30">
+
+    <div class="card-image">
+      <img src="https://materializecss.com/images/sample-1.jpg">
+      <span class="card-title">Card Title</span>
+    </div>
+    <div class="card-content">
+      <p>I am a very simple card. I am good at containing small bits of information.
+      I am convenient because I require little markup to use effectively.</p>
+    </div>
+
   </card-conteudo-vue>
 </template>
 
 <script>
   import CardConteudoVue from '@/components/social/CardConteudoVue'
-export default {
-  name: "Home",
-  components: {
-    CardConteudoVue
-  },
-  data() {
-    return {};
-  }
-};
+  export default {
+    name: "Home",
+    components: {
+      CardConteudoVue
+    },
+    data() {
+      return {};
+    }
+  };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
