b05872534f56d5127c35048e14172ed3b6e61e28
Eric Nakawatase <e.nakawatase@coblue.com.br>
2018-06-23 14:17:10 -0300

    23. Formulário de Login e Cadastro

--- src/pages/login/Login.vue
+++ src/pages/login/Login.vue
@@ -1,42 +1,54 @@
 <template>
 
   <login-template-vue>
-    
+
     <span slot="menuesquerdo">
       <img src="https://static.quizur.com/i/b/5b035c77702c13.471891555b035c775c3ff9.54480388.png" alt="" class="responsive-img">
     </span>
 
     <span slot="principal">
-    <h2>Login</h2>
 
-    <input type="text" placeholder="E-mail" value="">
-    <input type="text" placeholder="Password" value="">
-    <button type="button" class="btn">Entrar</button>
+      <span v-if="!cadastro">
+        <h2>Login</h2>
+
+        <input type="text" placeholder="E-mail" value="">
+        <input type="password" placeholder="Senha" value="">
+        <button class="btn">Entrar</button>
+        <button class="btn orange" v-on:click="cadastro = !cadastro">Cadastre-se</button>
+      </span>
+
+      <span v-if="cadastro">
+        <h2>Cadastro</h2>
+
+        <input type="text" placeholder="Nome" value="">
+        <input type="text" placeholder="E-mail" value="">
+        <input type="password" placeholder="Senha" value="">
+        <input type="password" placeholder="Confirme sua senha" value="">
+        <button class="btn">Enviar</button>
+        <button class="btn orange" v-on:click="cadastro = !cadastro">Já tenho conta</button>
+      </span>
+
     </span>
 
-  </login-template-vue>
+  </span>
+
+</login-template-vue>
 
 </template>
 
 <script>
 
   import LoginTemplateVue from '@/templates/LoginTemplateVue'
-  import SiteTemplateVue from '@/templates/SiteTemplateVue'
-  import CardConteudoVue from '@/components/social/CardConteudoVue'
-  import CardDetalheVue from '@/components/social/CardDetalheVue'
-  import PublicarConteudoVue from '@/components/social/PublicarConteudoVue'
 
   export default {
     name: "Login",
     components: {
-      LoginTemplateVue,
-      SiteTemplateVue,
-      CardConteudoVue,
-      CardDetalheVue,
-      PublicarConteudoVue
+      LoginTemplateVue
     },
     data() {
-      return {};
+      return {
+        cadastro: false
+      };
     }
   };
 </script>
